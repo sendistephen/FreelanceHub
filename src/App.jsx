6e17d2df7f904fa5a60355cc1c84921817e6d5ee
sendistephen <sendi.stev@gmail.com>
2024-08-03 18:32:07 +0300

    refactor project structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-green-500">Hello crypto</h1>
+    </div>
+  );
+}
+
+export default App;
